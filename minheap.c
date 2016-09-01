91cd50cde7c8965570885a34f1eb8576aa01d424
jacktang <jack at zzfs>
2016-09-01 16:49:54 +0800

    1

---
+++ minheap.c
@@ -0,0 +1,15 @@
+#include "minheap.h"
+#include <string.h>
+
+int push_heap(void *node, struct minheap *heap)
+{
+	return (0);
+}
+void *pop_heap(struct minheap *heap)
+{
+	return (NULL);
+}
+int adjust_heap_node(struct minheap *heap, void *node)
+{
+	return (0);
+}
